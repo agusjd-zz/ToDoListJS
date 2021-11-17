d26f7e847558b247aa0d6c2a3f912341264164c9
Agustin Diaz <agusjd92@gmail.com>
2021-11-17 01:15:11 -0300

    primer entrega de proyecto

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// class Tarea {
+//     constructor(id,nombre,texto) {
+//       this.id = id;
+//       this.nombre = nombre;
+//       this.texto = texto;
+//       this.finalizada = false;
+//     }
+
+//     finalizarTarea(){
+//       this.finalizada = true;
+//     }
+//   }
+
+// const tareas = [];
+ 
+// const tarea1 = new Tarea(1,"Tarea1","Texto de tarea1")
+// const tarea2 = new Tarea(1,"Tarea2","Texto de tarea2")
+
+// tareas.push(tarea1)
+// tareas.push(tarea2)
+// console.log(tarea1)
+// tarea2.finalizarTarea()
+// console.log(tareas)
+
+
+let listaTarea = []
+let tarea = prompt("Ingrese la tarea pendiente");
+listaTarea.push(tarea)
+let tarea2 = prompt("Ingrese la tarea pendiente");
+listaTarea.push(tarea2)
+let cantidadDeTareas = listaTarea.length
+console.log(listaTarea)
+console.log("Cantidad de tareas pendientes: " , cantidadDeTareas)
